a1edb40ad598c65c6bfe760801c2adc60475e069
Alessio Ragno <spideralessio97@gmail.com>
2017-07-26 16:43:35 +0200

    Increase DB

--- server/app.py
+++ server/app.py
@@ -3,6 +3,14 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import models
+
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
+
+    return d
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./app.db'
 
@@ -17,20 +25,20 @@ api = Api(app)
 # shows a single diagnosis item
 class Diagnosis(Resource):
 	def get(self, diagnosis_id):
-		return models.Diagnosis.query.get(1)
+		return row2dict(models.Diagnosis.query.get(diagnosis_id))
 
 # DiagnosisList
 # shows a list of diagnosis
 class DiagnosisList(Resource):
 	def get(self):
-		return models.Diagnosis.query.all()
+		return [row2dict(a) for a in models.Diagnosis.query.all()]
 
 
 # Drug
 # shows a single drug item
 class Drug(Resource):
 	def get(self, drug_id):
-		return {"N":models.Drug.query.get(1).recipe}
+		return row2dict(models.Drug.query.get(drug_id))
 
 
 # DrugList
@@ -42,15 +50,26 @@ class DrugList(Resource):
 		self.args = self.reqparse.parse_args()
 	def get(self):
 		print(self.args)
-		return models.Drug.query.all()
+		return [row2dict(a) for a in models.Drug.query.all()]
 	
 
+class Load(Resource):
+	def get(self):
+		#db.create_all()
+		for d in ["PIPPO", "TEST", "MA CHE NE SO", "CUORE MATTO"]:
 
+			diagnosis = models.Diagnosis(d)
+			db.session.add(diagnosis)
+		db.session.commit()
+		return True
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(DrugList, '/drugs')
+api.add_resource(Load, '/load')
 api.add_resource(Drug, '/drugs/<drug_id>')
+api.add_resource(DiagnosisList, '/diagnosis')
+api.add_resource(Diagnosis, '/diagnosis/<diagnosis_id>')
 
 
 if __name__ == '__main__':
